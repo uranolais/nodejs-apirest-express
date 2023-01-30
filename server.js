57db7bb3fc6aec576f1c532daa83b81e2cd20677
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-01-30 14:41:55 -0300

    Adicionando arquivos da aula01

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require("http")
+const port = 3000;
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros':'Entrei na pagina de livros',
+    '/autores': 'Listagem de autores',
+    '/editora': 'Pagina de Editora',
+    '/sobre': 'Info sobre o projeto',
+}
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(rotas[req.url]);
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
