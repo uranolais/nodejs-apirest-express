eadf27bb3368c3979f986fef5c0c18a193874664
Laís Urano <95968249+uranolais@users.noreply.github.com>
2023-01-30 16:03:55 -0300

    Adicionando aula02

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require("http")
-const port = 3000;
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros':'Entrei na pagina de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pagina de Editora',
-    '/sobre': 'Info sobre o projeto',
-}
+import app from './src/app.js'
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
